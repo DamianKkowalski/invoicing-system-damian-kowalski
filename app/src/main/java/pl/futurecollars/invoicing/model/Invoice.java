1000f612525b37b43f090811c35dd6891607b2fa
Damian Kowalski <damkow3847@gmail.com>
2024-04-02 13:58:59 +0200

    Lekcja 3 testy + funkcjonalnosci dodane

--- app/src/main/java/pl/futurecollars/invoicing/model/Invoice.java
+++ app/src/main/java/pl/futurecollars/invoicing/model/Invoice.java
@@ -4,8 +4,6 @@ import java.time.LocalDate;
 import java.util.List;
 import lombok.Data;
 
-
-
 @Data
 public class Invoice {
 
--- app/src/main/java/pl/futurecollars/invoicing/service/InvoiceService.java
+++ app/src/main/java/pl/futurecollars/invoicing/service/InvoiceService.java
@@ -6,11 +6,13 @@ import pl.futurecollars.invoicing.db.Database;
 import pl.futurecollars.invoicing.model.Invoice;
 
 public class InvoiceService {
+
   private final Database database;
 
   public InvoiceService(Database database) {
     this.database = database;
   }
+
   public int save(Invoice invoice) {
     return database.save(invoice);
   }
