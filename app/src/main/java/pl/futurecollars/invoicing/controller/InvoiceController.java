c52831495387c3d430ba4113cb4399d755143cac
Damian Kowalski <damkow3847@gmail.com>
2024-04-17 13:48:28 +0200

    Lekcja 7 Funcionality added

--- app/src/main/java/pl/futurecollars/invoicing/controller/InvoiceController.java
+++ app/src/main/java/pl/futurecollars/invoicing/controller/InvoiceController.java
@@ -1,6 +1,7 @@
 package pl.futurecollars.invoicing.controller;
 
 import java.util.List;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 import pl.futurecollars.invoicing.model.Invoice;
 import pl.futurecollars.invoicing.service.InvoiceService;
 
+@Slf4j
 @RestController
 @RequestMapping("invoices")
 public class InvoiceController {
@@ -32,11 +34,13 @@ public class InvoiceController {
 
   @PostMapping
   public int add(@RequestBody Invoice invoice) {
+    log.info("Używam postMapping update - info");
     return invoiceService.save(invoice);
   }
 
   @GetMapping(value = "/{id}", produces = { "application/json;charset=UTF-8" })
   public ResponseEntity<Invoice> getById(@PathVariable int id) {
+    log.info("Używam getMapping update - info");
     return invoiceService.getById(id)
         .map(invoice -> ResponseEntity.ok().body(invoice))
         .orElse(ResponseEntity.notFound().build());
@@ -44,6 +48,7 @@ public class InvoiceController {
 
   @DeleteMapping("/{id}")
   public ResponseEntity<?> deleteById(@PathVariable int id) {
+    log.info("Używam deleteMapping update - info");
     return invoiceService.delete(id)
         .map(name -> ResponseEntity.noContent().build())
         .orElse(ResponseEntity.notFound().build());
@@ -51,9 +56,11 @@ public class InvoiceController {
 
   @PutMapping("/{id}")
   public ResponseEntity<?> update(@PathVariable int id, @RequestBody Invoice invoice) {
+    log.info("Używam putMapping update - info");
     return invoiceService.update(id, invoice)
         .map(name -> ResponseEntity.noContent().build())
         .orElse(ResponseEntity.notFound().build());
+
   }
 }
 
