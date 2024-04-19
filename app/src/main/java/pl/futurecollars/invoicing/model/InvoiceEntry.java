d62bdd449c7a4d0982263cd5504df13ed00317c0
Damian Kowalski <damkow3847@gmail.com>
2024-04-19 16:11:28 +0200

    Lesson 8  Swagger implementation

--- app/src/main/java/pl/futurecollars/invoicing/model/InvoiceEntry.java
+++ app/src/main/java/pl/futurecollars/invoicing/model/InvoiceEntry.java
@@ -1,5 +1,6 @@
 package pl.futurecollars.invoicing.model;
 
+import io.swagger.annotations.ApiModelProperty;
 import java.math.BigDecimal;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -10,14 +11,18 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class InvoiceEntry {
 
+  @ApiModelProperty(value = "Product/service description", required = true, example = "Laptop")
   private String description;
 
+  @ApiModelProperty(value = "Number of items", required = true, example = "85")
   private int quantity;
 
+  @ApiModelProperty(value = "Product/service netto price", required = true, example = "555")
   private BigDecimal price;
 
+  @ApiModelProperty(value = "Product/service tax value", required = true, example = "123")
   private BigDecimal vatValue;
-
+  @ApiModelProperty(value = "Tax rate", required = true)
   private Vat vatRate;
 
 }
