24a6a5d3fee512cb748825ec08fb4d839bca0bb3
DamianKkowalski <146637824+DamianKkowalski@users.noreply.github.com>
2024-04-19 19:18:00 +0200

    Lesson8 (#9)
    * Lesson 8  Swagger implementation
    
    * Lesson 8  Swagger implementation
    
    * Lesson 8  Swagger implementation

--- app/src/main/java/pl/futurecollars/invoicing/model/Company.java
+++ app/src/main/java/pl/futurecollars/invoicing/model/Company.java
@@ -1,5 +1,6 @@
 package pl.futurecollars.invoicing.model;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -7,8 +8,13 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class Company {
 
+  @ApiModelProperty(value = "Tax Identification number", required = true, example = "123-123-123-12")
   private String taxIdentifications;
+  @ApiModelProperty(value = "Company address", required = true, example = "ul. Słowianska 123;  33-333 Zielona Gora")
+
   private String address;
+  @ApiModelProperty(value = "Company name", required = true, example = "KowalPol.sp. z o. o.")
+
   private String name;
 
   public Company(String taxIdentifications, String address, String name) {
