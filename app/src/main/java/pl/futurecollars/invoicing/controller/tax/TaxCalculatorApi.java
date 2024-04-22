fe97b3d32f72619cb77e53b231278a6869dc6f2e
Damian Kowalski <damkow3847@gmail.com>
2024-04-22 15:04:13 +0200

    Lesson 9  Tax calculator

---
+++ app/src/main/java/pl/futurecollars/invoicing/controller/tax/TaxCalculatorApi.java
@@ -0,0 +1,19 @@
+package pl.futurecollars.invoicing.controller.tax;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import pl.futurecollars.invoicing.service.TaxCalculatorResult;
+
+@RequestMapping("tax")
+@Api(tags = {"tax - controller"})
+public interface TaxCalculatorApi {
+
+  @ApiOperation(value = "Get incomes, costs, vat nad taxes to pay")
+  @GetMapping(value = "/{taxIdentificationNumber}", produces = {"application/json;charset=UTF-8"})
+  TaxCalculatorResult calculateTaxes(@PathVariable @ApiParam(example = "333-333-33-33") String taxIdentificationNumber);
+
+}
--- app/src/test/groovy/pl/futurecollars/invoicing/model/InvoiceTest.groovy
+++ app/src/test/groovy/pl/futurecollars/invoicing/model/InvoiceTest.groovy
@@ -8,22 +8,24 @@ class InvoiceTest extends Specification {
 
     def "should create invoice with data" () {
         given:
-        def date = LocalDate.now()
-        def buyer = new Company(name: "bb", taxIdentifications: "213123123")
-        def seller = new Company(name: "bb", taxIdentifications: "213123123")
-        def entries = [
-                new InvoiceEntry(description: "ddd", price: 20, vatValue: 4.0, vatRate: Vat.VAT_0),
-                new InvoiceEntry(description: "ddd", price: 10, vatValue: 3.0, vatRate: Vat.VAT_8)
-        ]
-        when:
-        def invoice = new Invoice(date, buyer, seller, entries)
+        def invoiceEntry = InvoiceEntry.builder()
+                .description("Laptop")
+                .quantity(1)
+                .price(new BigDecimal("1000.00"))
+                .vatValue(new BigDecimal("230.00"))
+                .vatRate(Vat.VAT_23).build()
 
-        then:
-        invoice.date == date
-        invoice.buyer == buyer
-        invoice.seller == seller
-        invoice.entries == entries
+        def invoice = Invoice.builder()
+                .date(LocalDate.now())
+                .buyer(new Company(name: "bb", taxIdentifications: "213123123"))
+                .seller(new Company(name: "bb", taxIdentifications: "213123123"))
+                .entries([invoiceEntry]).build()
 
+        expect:
+        invoice.entries != null
+        invoice.seller != null
+        invoice.buyer != null
+        invoice.date != null
 
     }
 
