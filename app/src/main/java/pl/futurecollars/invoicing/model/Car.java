09b40ea04e4d41748e08d51ad5f2db4c3827b330
DamianKkowalski <146637824+DamianKkowalski@users.noreply.github.com>
2024-04-25 21:07:12 +0200

    Lesson10 (#12)
    * Lesson 10  Extendiding tax calculator
    
    * Lesson 10  Extendiding tax calculator
    
    * Lesson 10  Extendiding tax calculator

---
+++ app/src/main/java/pl/futurecollars/invoicing/model/Car.java
@@ -0,0 +1,22 @@
+package pl.futurecollars.invoicing.model;
+
+import io.swagger.annotations.ApiModelProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+
+public class Car {
+
+  @ApiModelProperty(value = "Car registraion number", required = true, example = "EPI 123456")
+private String registrationNumber;
+
+  @ApiModelProperty(value = "Specifies if car is used for personal reasons", required = true, example = "true")
+private boolean personalUse;
+
+}
--- app/src/test/groovy/pl/futurecollars/invoicing/controller/AbstractControllerTest.groovy
+++ app/src/test/groovy/pl/futurecollars/invoicing/controller/AbstractControllerTest.groovy
@@ -5,9 +5,11 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.http.MediaType
 import org.springframework.test.web.servlet.MockMvc
+import pl.futurecollars.invoicing.model.Company
 import pl.futurecollars.invoicing.model.Invoice
 import pl.futurecollars.invoicing.service.TaxCalculatorResult
 import pl.futurecollars.invoicing.utils.JsonService
+import spock.lang.Specification
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
@@ -19,7 +21,7 @@ import static pl.futurecollars.invoicing.TestHelpers.invoice
 
 @AutoConfigureMockMvc
 @SpringBootTest
-class AbstractControllerTest {
+class AbstractControllerTest extends Specification{
 
     static final String INVOICE_ENDPOINT = "/invoices"
     static final String TAX_CALCULATOR_ENDPOINT = "/tax"
@@ -39,11 +41,11 @@ class AbstractControllerTest {
                 .andExpect(status().isNoContent())
     }
 
-    int addInvoiceAndReturnId(String invoiceAsJson) {
+    int addInvoiceAndReturnId(Invoice invoice) {
         Integer.valueOf(
                 mockMvc.perform(
                         post(INVOICE_ENDPOINT)
-                                .content(invoiceAsJson)
+                                .content(jsonService.toJson(invoice))
                                 .contentType(MediaType.APPLICATION_JSON)
                 )
                         .andExpect(status().isOk())
@@ -76,7 +78,7 @@ class AbstractControllerTest {
     List<Invoice> addUniqueInvoices(int count) {
         (1..count).collect { id ->
             def invoice = invoice(id)
-            invoice.id = addInvoiceAndReturnId(jsonService.toJson(invoice))
+            invoice.id = addInvoiceAndReturnId(invoice)
             return invoice
         }
     }
@@ -85,14 +87,17 @@ class AbstractControllerTest {
         jsonService.toJson(invoice(id))
     }
 
-    TaxCalculatorResult calculateTax(String taxIdentificationNumber) {
-        def response = mockMvc.perform(get("$TAX_CALCULATOR_ENDPOINT/$taxIdentificationNumber"))
+    TaxCalculatorResult calculateTax(Company company) {
+        def response = mockMvc.perform(
+                post("$TAX_CALCULATOR_ENDPOINT")
+                        .content(jsonService.toJson(company))
+                        .contentType(MediaType.APPLICATION_JSON)
+        )
                 .andExpect(status().isOk())
                 .andReturn()
                 .response
                 .contentAsString
-
-        jsonService.toObject(response, TaxCalculatorResult)
+        jsonService.toObject(response, TaxCalculatorResult.class)
     }
 
 }
